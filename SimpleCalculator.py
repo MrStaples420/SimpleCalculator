cc450bccaeb00015701b8b960f748a401d9c93ae
MrStaples <69106822+MrStaples420@users.noreply.github.com>
2022-09-20 22:16:53 -0700

    Add files via upload

--- SimpleCalculator.py
+++ SimpleCalculator.py
@@ -1,20 +1,33 @@
-print("1.Addition\n"
-      "2.Subtraction\n"
-      "3.Multiplication\n"
-      "4.Division\n")
-# you get to choose what operator you want to use
-Op = int(input("What would you want to do. Choose Numbers 1-4: "))
-num1 = int(input("Enter number: "))
-num2 = int(input("Enter another number: "))
 
-# setting the algorithm you want to use
-if Op == 1:
-    print(num1 + num2)
-elif Op == 2:
-    print(num1 - num2)
-elif Op == 3:
-    print(num1 * num2)
-elif Op == 4:
-    print(num1 / num2)
-else:
-    print("Invalid Option. Choose between 1-4: ")
+
+def main():
+    print("1.add\n2.sub\n3.mult\n4.div")
+
+    op = int(input("enter a number between 1-4: "))
+    num1 = int(input("enter a number: "))
+    num2 = int(input("enter another number: "))
+
+    if op == 1:
+        print(num1 + num2)
+    elif op == 2:
+        print(num1 - num2)
+    elif op == 3:
+        print(num1 * num2)
+    elif op == 4:
+        print(num1 / num2)
+    else:
+        print("you didn't choose numbers 1-4 pls do so!")
+
+
+def redo():
+
+    restart = (int(input("do you want to continue 1 for yes 2 for no: ")))
+
+    while restart == 1:
+        main()
+        redo()
+    exit()
+
+
+main()
+redo()
